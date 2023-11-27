8fe543c148d869815dc27e9fb4f4c78d4b78bfef
mtnowl <henry@cadenzadevelopment.com>
2023-11-27 15:47:14 +0800

    Force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Consumer } from './consumer';
 import Loading from './loading';
 import { Suspense } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   return (
     <main className={styles.main}>
